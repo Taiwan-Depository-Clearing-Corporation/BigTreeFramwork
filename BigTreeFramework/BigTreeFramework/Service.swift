3f8ec862bb1b2525689ef60e4a2b84d879caed6a
rick <>
2020-02-10 15:38:03 +0800

    First commit

---
+++ BigTreeFramework/BigTreeFramework/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  BigTreeFramework
+//
+//  Created by WEI on 2020/2/10.
+//  Copyright © 2020 greattree. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init(){
+        
+    }
+    
+    public static func doSomething() -> String {
+        return " framework test"
+    }
+}
